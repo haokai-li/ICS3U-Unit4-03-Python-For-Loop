c767220b9e16d08da5a92350281f3a613ad70de7
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-01 16:10:32 +0000

    for loop

---
+++ for_loop.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This Program is about for loop
+
+
+def main():
+    # This function is about for loop
+    loop_number = 0
+    answer_number = 0
+
+    # input
+    user_string = input("Please enter a positive integer: ")
+    print("")
+
+    # process
+    try:
+        user_number = int(user_string)
+        if user_number >= 0:
+            for loop_number in range(user_number + 1):
+                answer_number = loop_number ** 2
+                # output
+                print("{0}² = {1}.".format(loop_number, answer_number))
+        else:
+            print("You didn't enter a positive integer.")
+    except Exception:
+        # output
+        print("You didn't enter an integer.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
